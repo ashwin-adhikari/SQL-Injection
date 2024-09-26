42a1f4ea277c4281d340f0f78ab6649a49db588b
Ashwin Adhikari <adhikariaswin7@gmail.com>
2024-09-26 18:00:00 +0545

    script lab#1

--- Labs/Lab1 - Retrieving hidden data.py
+++ Labs/Lab1 - Retrieving hidden data.py
@@ -1,28 +1,16 @@
 """
 SQL injection vulnerability in WHERE clause allowing retrieval of hidden data
 """
-from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import requests
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 
+import os
 
-# url = r'https://portswigger.net/academy/labs/launch/13e48d1949abb8793e11da34ed06a5811ea3a9b2be0501a5e9f0deb255d37406?referrer=%2fweb-security%2fsql-injection%2fexamining-the-database%2flab-querying-database-version-mysql-microsoft'
 
-# soup = BeautifulSoup(html, 'lxml')
-# filters = soup.find('section', class_ = "search-filters")
-# print(filters)
-
-driver = webdriver.Edge()
-
-driver.get('https://portswigger.net/academy/labs/launch/13e48d1949abb8793e11da34ed06a5811ea3a9b2be0501a5e9f0deb255d37406?referrer=%2fweb-security%2fsql-injection%2fexamining-the-database%2flab-querying-database-version-mysql-microsoft')
-
-element = driver.find_element(By.ID, 'sb_form_q')
-element.send_keys('WebDriver')
-element.submit()
-print(element)
-time.sleep(5)
-driver.quit()
+driver = webdriver.Firefox('C:/Users/Ripple/Downloads/geckodriver-v0.35.0-win64/geckodriver.exe')
+driver.get("https://portswigger.net/web-security/sql-injection/lab-retrieve-hidden-data")
